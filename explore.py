62de1e36e3c510d04df16b28e22e74573a8839e1
Dant86 <vedantdpathak@iCloud.com>
2018-08-11 14:31:27 -0500

    sets up data

--- explore.py
+++ explore.py
@@ -1,4 +1,7 @@
+from conceptnet5.vectors.query import VectorSpaceWrapper
+from nltk.tokenize import RegexpTokenizer
 import pandas as pd
+import numpy as np
 
 df = pd.read_json("train.json")
 
@@ -38,3 +41,55 @@ def remove_common_from_rec(ingreds):
 
 print("Removing {} ingredients from all recipes...".format(one_perc))
 df.ingredients.apply(remove_common_from_rec)
+
+'''
+    separate all ingredients into 1-word tokens
+'''
+tokenizer = RegexpTokenizer(r'\w+')
+
+
+# separating
+def separate_ingredients(ingreds):
+    new_ingreds = []
+    for ingred in ingreds:
+        for word in tokenizer.tokenize(ingred):
+            new_ingreds.append(word)
+        ingreds.remove(ingred)
+    return new_ingreds
+
+
+separated = [separate_ingredients(recipe) for recipe in df.ingredients]
+df["separated_ingredients"] = separated
+
+'''
+    Use separated ingredients to create feature vectors
+'''
+wrapper = VectorSpaceWrapper("../Vectors/mini.h5")
+
+
+# using conceptnet to create feature vectors
+def create_feature_vector(recipe):
+    vecs = []
+    for ingred in recipe:
+        vecs.append(wrapper.text_to_vector("en", ingred))
+    return sum(vecs)
+
+
+inputs = [create_feature_vector(recipe) for recipe in df.separated_ingredients]
+print("Input vectors are of shape {}".format(np.shape(inputs[0]))) # (300,)
+
+
+'''
+    Transform outputs into vectors
+'''
+
+
+# one-hot encoding outputs
+def cuisine_to_vec(cuisine):
+    out = np.array((amt_cuisines,))
+    out[cuisines.index(cuisine)] = 1
+    return out
+
+
+outputs = [cuisine_to_vec for cuisine in df.cuisines]
+print(outputs[0])
